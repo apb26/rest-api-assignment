402920fe0e219ebbf7406c965f51591f72967e02
Alex Berg <apb@njit.edu>
2025-09-23 20:17:54 -0400

    add POST and GET methods for testing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const { v4: uuidv4 } = require('uuid'); 
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -8,6 +9,28 @@ app.use(express.json());
 // **************************************************************
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
+
+let users = [];
+
+app.post('/users', (req, res) => {
+    const {name, email} = req.body;
+
+    const newUser = {
+        id: uuidv4().split('-')[0], 
+        name, 
+        email};
+
+        users.push(newUser);
+
+        res.status(201).json(newUser);
+});
+
+app.get('/users/:id', (req, res) => {
+    const {id} = req.params
+    const user = users.find(u => u.id == id);
+
+    res.status(200).json(user);
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
